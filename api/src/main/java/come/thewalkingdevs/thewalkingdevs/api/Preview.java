78a7734dc43c4ab0ebe5b9f9a74d59e02dadcbed
trogdan <mr.trogdan@gmail.com>
2015-11-09 21:21:57 -0800

    Revert "New things"
    This reverts commit 18104b95484f1e4180e5db91bbe61216a317b779.

--- api/src/main/java/come/thewalkingdevs/thewalkingdevs/api/Preview.java
+++
@@ -1,7 +0,0 @@
-package come.thewalkingdevs.thewalkingdevs.api;
-
-/**
- * Created by levani on 11/9/15.
- */
-public class Preview {
-}
--- api/src/main/java/come/thewalkingdevs/thewalkingdevs/api/constants/AppConstants.java
+++
@@ -1,7 +0,0 @@
-package come.thewalkingdevs.thewalkingdevs.api.constants;
-
-/**
- * Created by levani on 11/9/15.
- */
-public class AppConstants {
-}
